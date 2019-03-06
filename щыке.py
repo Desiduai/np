81016c71652063c0197e68aec9927542659f1d32
Desiduai <43550549+Desiduai@users.noreply.github.com>
2019-03-06 13:07:02 +0300

    Add files via upload

--- щыке.py
+++ щыке.py
@@ -7,30 +7,25 @@ M = int(input('Введите кол-во строк: '))
 a = np.random.randint(-5,5,(N,M))
 b = np.random.randint(-5,5,(N,M))
 c = np.random.randint(-5,5,(int(input('Введите длину вектора: '))))
-d = np.random.randint(-5,5,(N,N))
+D = np.random.randint(-5,5,(N,N))
 t.sleep(1)
 print('\n',a)
 print('\n',b)
 print('\n',c)
-print('\n',d)
+print('\n',D)
 
 def det():
-    print('\nОпределитель матрицы D: ',np.linalg.det(d))
+    print('\nОпределитель матрицы D: ',np.linalg.det(D))
 
 def count():
-    k = 0
-    k1 = 0
-    for i in range(len(d)):
-        for j in range(len(d[i])):
-            if (d[i,j] < 0):
-                k += 1
-            elif (d[i,j] > 0):
-                k1 += 0
-    for i in range(len(d)):
-        for j in range(len(d[i])):
-            if (k > k1):
-                d[i,j] *= -1
-    print('\n',d)
+    k = np.count_nonzero(np.where(D < 0, D, None))
+    k1 = np.count_nonzero(np.where(D > 0, D, None))
+    print(k)
+    print(k1)
+
+    if (k > k1):
+        d = np.where(D, D * (-1), None)
+    print('\n', D)
 
 def mult():
     K = int(input('Введите число на которое будент умножена матрица A: '))
@@ -57,11 +52,12 @@ def raise_in_power():
     print('\n',np.power(b,3))
 
 def check_d():
+    print(np.linalg.det(D))
     try:
-        np.linalg.det(b) != 0
-    except:
-        print('Ooops. This matrix is singular')
-    print('\n',np.linalg.inv(d))
+        print('\n', np.linalg.inv(D))
+    except BaseException:
+        print('Oops. This matrix is singular')
+
 
 print('\nВызов нужной вам функции...')
 print('\nВыберите нужное: (1)Поиск определителя матрицы b. (2)Вынос минуса за "скобки". (3)Умножения матрицы A на число. (4)Умножения и сложения матрицы. (5)Проверка тождеств. (6)Возведение матриц в степень. (7)Нахождение обратной матрицы.')
